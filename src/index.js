33db09bcdaa46193242521b82a02854e055843f4
Akash Tiwari <drakashti@gmail.com>
2019-11-13 18:10:45 +0530

    Evening 13/11/2019

--- src/index.js
+++ src/index.js
@@ -1,50 +1,44 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import ToDo from "./ToDo"
-import ToDoMap from "./ToDoMap"
+import ToDo from "./ToDo";
+import ToDoMap from "./ToDoMap";
 import "./styles.css";
 
-
-class App extends React.Component{
-
-  constructor(){
-    super();
-    this.state={
-     id:1,
-    name:"sky"
+class App extends React.Component {
+  state = {
+    ct: 0
+  };
+  Handle() {
+    this.setState({ ct: 2 });
   }
-  }
+  render() {
+    const todoItems = ToDoMap.map(items => (
+      <ToDo item={items.item} id={items.id} />
+    ));
 
-  render(){
-const todoItems=ToDoMap.map((items)=>
-<ToDo item={items.item} id={items.id}/>
-)
-
-    return(
+    return (
       <div>
-       
+        <p>{this.state.ct}</p>
+        <br />
+        <button onClick={this.Handle}>Click Me</button>
         {todoItems}
         <h1>Id: {this.state.id}</h1>
-        <Header username={this.state.name}/>
-        <Greeting/>
+        <Header username={this.state.name} />
+        <Greeting />
       </div>
-    )
+    );
   }
 }
 
-class Header extends React.Component{
-  render(){
-    return(
-     <p>Hi this header {this.props.username}</p>
-    )
+class Header extends React.Component {
+  render() {
+    return <p>Hi this header {this.props.username}</p>;
   }
 }
 
-class Greeting extends Component{
-  render(){
-    return(
-      <p>Hi this is footer</p>
-    )
+class Greeting extends Component {
+  render() {
+    return <p>Hi this is footer</p>;
   }
 }
 
